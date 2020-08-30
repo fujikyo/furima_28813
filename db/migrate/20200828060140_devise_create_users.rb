f1f84d8109882762342ddf654407254d8cebce66
fujikyo <guitar0324@gmail.com>
2020-08-30 17:00:19 +0900

    マイグレーションファイル記述

--- db/migrate/20200828060140_devise_create_users.rb
+++ db/migrate/20200828060140_devise_create_users.rb
@@ -5,13 +5,14 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
     create_table :users do |t|
       ## Database authenticatable
       t.string :nickname, null: false
-      t.string :email,null: false
-      ｔ.string :password,null: false
       ｔ.string :firstname_hira,null: false
       ｔ.string :lastname_hira,null: false
       ｔ.string :firstname_kana,null: false
       ｔ.string :lastname_kana,null: false
       ｔ.date :birthday,null: false
+
+      t.string :email,              null: false, default: ""
+      t.string :encrypted_password, null: false, default: ""
 
 
       ## Recoverable
@@ -43,8 +44,8 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
       t.timestamps null: false
     end
 
-    # add_index :users, :email,                unique: true
-    # add_index :users, :reset_password_token, unique: true
+    add_index :users, :email,                unique: true
+    add_index :users, :reset_password_token, unique: true
     # add_index :users, :confirmation_token,   unique: true
     # add_index :users, :unlock_token,         unique: true
   end
