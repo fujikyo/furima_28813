63ad46a454e48d05da7949f14d0875673843071b
fujikyo <guitar0324@gmail.com>
2020-09-03 16:22:06 +0900

    rootingとcontroller仮置き、newへのリンク記述

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,16 @@
 class ItemsController < ApplicationController
   def index
   end
+
+  def new
+  end
+
+  def create
+    Item.create(sell_items)
+  end
+
+  private
+  def sell_items
+    params.require(:item).permit()
+  end
 end
