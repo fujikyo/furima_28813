75a15eb09a3cb68e8bd3afe569215f863b725470
fujikyo <guitar0324@gmail.com>
2020-09-07 12:29:49 +0900

    修正　非同期計算の変数名変更

--- app/javascript/culc.js
+++ app/javascript/culc.js
@@ -3,8 +3,8 @@ const inputtedPrice = document.getElementById("item-price").value;
 const taxPrice = Math.round(inputtedPrice * 0.1);
 const profitPrice = inputtedPrice - taxPrice;
 
-const a = document.getElementById("add-tax-price")
-const b = document.getElementById("profit")
-a.innerHTML = taxPrice
-b.innerHTML = profitPrice
+const getTaxClass = document.getElementById("add-tax-price")
+const getProfitClass = document.getElementById("profit")
+getTaxClass.innerHTML = taxPrice
+getProfitClass.innerHTML = profitPrice
 })
