00964b518e898d6391800ecfe027c1b56124f75a
fujikyo <guitar0324@gmail.com>
2020-09-16 12:03:11 +0900

    pay jp 決済機能のみ実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -6,7 +6,8 @@ class OrdersController < ApplicationController
   end
 
   def create
-    @order = Order.new(price: order_params[:price])
+    @item = Item.find(params[:item_id])  
+    @order = Order.new(order_params)
     if @order.valid?
       pay_item
       @order.save
@@ -23,9 +24,9 @@ class OrdersController < ApplicationController
   end
 
   def pay_item
-    Payjp.api_key = "sk_test_XXXXXXXX"  # PAY.JPテスト秘密鍵
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"] # PAY.JPテスト秘密鍵
     Payjp::Charge.create(
-      amount: order_params[:price],  # 商品の値段
+      amount: @item.price,           # 商品の値段
       card: order_params[:token],    # カードトークン
       currency:'jpy'                 # 通貨の種類(日本円)
     )
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,6 +1,8 @@
 class Order < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :area
+  attr_accessor :token
+  validates :price, :token, presence: true
   validates :postal_code, :prefecture_id, :municipality, :street_number, :phone, :purchase, presence: true
   validates :postal_code, format: { with: /\A\d{3}[-]\d{4}\z/ }
   validates :phone, format: { with: /\A\d{11}\z/ }
