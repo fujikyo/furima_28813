4b2f2758758800198042c919cda9a99181a59a1e
fujikyo <guitar0324@gmail.com>
2020-09-17 21:07:52 +0900

    修正　不要なコメントアウト削除、with_options使用

--- app/forms/purchase_order.rb
+++ app/forms/purchase_order.rb
@@ -1,14 +1,19 @@
 class PurchaseOrder
   include ActiveModel::Model
   attr_accessor :postal_code, :area_id, :municipality, :street_number, :building_name, :phone, :item_id, :user_id, :token, :price, :purchase_id, :token
-  # orderのバリデーション
-  validates :token, presence: true
-  validates :postal_code, :area_id, :municipality, :street_number, :phone, presence: true
-  validates :postal_code, format: { with: /\A\d{3}[-]\d{4}\z/ }
-  validates :phone, numericality: { only_integer: true }
-  validates :phone, length: { maximum: 11 }
-  # purchasesのバリデーション
-  validates :item_id, :user_id, presence: true
+  with_options presence: true do
+    validates :token
+    validates :postal_code
+    validates :area_id
+    validates :municipality
+    validates :street_number
+    validates :phone
+    validates :item_id
+    validates :user_id
+    validates :postal_code, format: { with: /\A\d{3}[-]\d{4}\z/ }
+    validates :phone, numericality: { only_integer: true }
+    validates :phone, length: { maximum: 11 }
+  end
 
   def save
     purchase = Purchase.create(item_id: item_id, user_id: user_id)
